62efe2d29709e6b59196c533e2d5d0262bac1f72
IvaSv <76878466+IVA78@users.noreply.github.com>
2024-10-19 10:28:43 +0200

    Definiranje entiteta, slanje upita u bazu i prikaz broja ulaznica na pocetnoj stranici

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import express, { Request, Response } from "express";
 import path from "path";
 import dotenv from "dotenv";
 import cors from "cors";
-import { Pool, PoolClient } from "pg";
+import { Pool, PoolClient, QueryResult } from "pg";
+import { BIGINT } from "sequelize";
 
 //konfiguracija env
 dotenv.config({
@@ -28,9 +29,86 @@ app.use(express.static(path.join(__dirname, "./public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+//slanje upita u bazu
+async function connect(
+  queryText: string,
+  params: any[] = []
+): Promise<QueryResult | null> {
+  let client: PoolClient | null = null;
+  try {
+    //povezivanje s bazom - dohvat instance veze
+    client = await pool.connect();
+    console.log("Connected to the database successfully");
+
+    const result = await client.query(queryText, params);
+    return result;
+  } catch (error) {
+    console.error("Failed to execute query:", error);
+    return null;
+  } finally {
+    if (client) {
+      client.release();
+    }
+  }
+}
+
+//inicijalno postavljanje baze
+async function databaseInit() {
+  const queryText1 = `CREATE TABLE IF NOT EXISTS ticket_owner (
+                        vatin BIGINT PRIMARY KEY, --OIB
+                        firstName VARCHAR(100) NOT NULL,
+                        lastName VARCHAR(100) NOT NULL
+                      `;
+  const queryText2 = `CREATE TABLE IF NOT EXISTS ticket (
+                        ticket_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                        owner_oib BIGINT NOT NULL,
+                        FOREIGN KEY (owner_oib) REFERENCES ticket_owner(vatin));`;
+  const queryText3 = `SELECT table_name
+                        FROM information_schema.tables
+                        WHERE table_schema = 'public';`;
+  //no params needed for any of the queries
+  await connect(queryText1);
+  await connect(queryText2);
+  const result = await connect(queryText3);
+
+  if (result) {
+    console.log("Query result:", result.rows);
+  } else {
+    console.log("Query execution failed.");
+  }
+}
+//databaseInit();
+
+//dohvat broja generiranih ulaznica iz baze
+async function getNumberOfTickets(): Promise<number | null> {
+  const queryText = `SELECT count(*) FROM ticket`;
+  const result = await connect(queryText);
+
+  if (result && result.rows.length > 0) {
+    const count = parseInt(result.rows[0].count, 10);
+    return count;
+  } else {
+    console.log("Failed to get the number of tickets.");
+    return null;
+  }
+}
+
 //definiranje osnovne rute za posluzivanje pocetne stranice
-app.get("/", (req: Request, res: Response) => {
-  res.render("index");
+app.get("/", async (req: Request, res: Response) => {
+  try {
+    //dohvat broja generiranih ulaznica
+    const count = await getNumberOfTickets();
+    console.log("count:", count);
+
+    //renderiranje broja ulaznica
+    res.render("index", {
+      ticketCount: count,
+    });
+  } catch (error) {
+    console.error("Error fetching ticket count:", error);
+    res.status(500).send("An error occurred while fetching the ticket count.");
+  }
 });
 
 //definiranje rute za prikaz podataka
@@ -41,24 +119,6 @@ app.get("/data", (req: Request, res: Response) => {
   };
   res.json(exampleData);
 });
-
-//povezivanje s bazom - instanciranje veze
-async function connect(): Promise<PoolClient | null> {
-  const client = await pool.connect();
-  try {
-    console.log("Connected to the database successfully");
-    const { rows } = await client.query("SELECT current_user");
-    const currentUser = rows[0]["current_user"];
-    console.log(`Current user: ${currentUser}`);
-    return client;
-  } catch (error) {
-    console.error("Failed to connect to the database:", error);
-    return null;
-  } finally {
-    client.release();
-  }
-}
-connect();
 
 //pokretanje klijentskog servera
 app.listen(port, () => {
