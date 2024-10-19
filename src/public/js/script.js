8389a2b2153855b46b1d24f154fa9cb11df331c6
IvaSv <76878466+IVA78@users.noreply.github.com>
2024-10-19 10:40:58 +0200

    Azuriranje javne pocetne stranice

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("JavaScript is working!");
-  alert("Welcome to My Web App!");
+  alert("Welcome to ticket QR code generator!");
 });
