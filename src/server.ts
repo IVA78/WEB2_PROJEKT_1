f961d335ddfe24496bbe926d518bc60531d6634b
IvaSv <76878466+IVA78@users.noreply.github.com>
2024-10-19 23:56:25 +0200

    Generiranje ulaznice i preusmjeravanje na stranicu sa podatcima vlasnika ulaznice

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import { auth } from "express-oauth2-jwt-bearer";
 import https from "https";
 import fs from "fs";
 import request from "request";
+import { v4 as uuidv4 } from "uuid";
+import QRCode from "qrcode";
 
 //konfiguracija env
 dotenv.config({
@@ -20,6 +22,7 @@ console.log("dirname: ", __dirname);
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 //konfiguracija baze
 const pool = new Pool({
@@ -59,15 +62,16 @@ async function connect(
 
 //inicijalno postavljanje baze
 async function databaseInit() {
+  //await connect("DROP TABLE ticket_owner");
+  //await connect("DROP TABLE ticket");
   const queryText1 = `CREATE TABLE IF NOT EXISTS ticket_owner (
-                        vatin BIGINT PRIMARY KEY, --OIB
+                        vatin VARCHAR(100) PRIMARY KEY, --OIB
                         firstName VARCHAR(100) NOT NULL,
-                        lastName VARCHAR(100) NOT NULL
-                      `;
+                        lastName VARCHAR(100) NOT NULL);`;
   const queryText2 = `CREATE TABLE IF NOT EXISTS ticket (
                         ticket_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                         created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-                        owner_oib BIGINT NOT NULL,
+                        owner_oib VARCHAR(100) NOT NULL,
                         FOREIGN KEY (owner_oib) REFERENCES ticket_owner(vatin));`;
   const queryText3 = `SELECT table_name
                         FROM information_schema.tables
@@ -161,10 +165,72 @@ app.post(
   async (req: CustomRequest, res: Response) => {
     try {
       //kontrolni ispis dobivenog tokena
-      console.log("Token: ", req.firstFunctionData.data.access_token);
+      //console.log("Token: ", req.firstFunctionData.data.access_token);
 
       //generiranje ulaznice
       console.log("Generiram ulaznicu!");
+
+      //dohvat podataka iz forme
+      const { OIB, firstName, lastName } = req.body;
+      console.log("Received form data:", { OIB, firstName, lastName });
+      //provjera podataka iz forme
+      if (!OIB || !firstName || !lastName) {
+        res.status(400).json({
+          message: "All fields are required: vatin, firstName, and lastName.",
+        });
+        return;
+      }
+
+      //spremi novog vlasnika ulaznice u bazu (ako vec ne postoji sa istim OIB-om)
+      const queryText1 = `INSERT INTO ticket_owner (vatin, firstName, lastName)
+                         VALUES ('${OIB}', '${firstName}', '${lastName}')
+                         ON CONFLICT (vatin) DO NOTHING;`;
+      await connect(queryText1);
+
+      //provjera broja ulaznica sa istim OIB-om (max 3)
+      const queryText2 = `SELECT COUNT(*)
+                          FROM ticket
+                          WHERE owner_oib = '${OIB}';`;
+      const result = await connect(queryText2);
+      const count: number = result ? parseInt(result.rows[0].count, 10) : -1;
+
+      if (count != -1) {
+        if (count >= 3) {
+          res
+            .status(400)
+            .json({ message: "Vatin already used for three tickets!" });
+          return;
+        } else {
+          //generiranje nove ulaznice
+
+          const queryText3 = `INSERT INTO ticket (owner_oib)
+                              VALUES ( ${OIB})
+                              RETURNING ticket_id`;
+          const result = await connect(queryText3);
+          const ticket_id = result ? result.rows[0].ticket_id : 0;
+          const created_at = result ? result.rows[0].created_at : 0;
+          console.log(`(${ticket_id}, ${created_at}, ${OIB})`);
+
+          try {
+            const ticketURL = `${req.protocol}://${req.get(
+              "host"
+            )}/ticket/${ticket_id}`;
+
+            const qrCodeImage = await QRCode.toBuffer(ticketURL);
+
+            // Konvertiranje buffera u Base64 string
+            const base64QRCode = qrCodeImage.toString("base64");
+
+            // slanje qr koda u pug template koji ce se renderirati
+            res.render("qr-code", { qrCode: base64QRCode });
+          } catch (error) {
+            console.error("Error generating QR code:", error);
+            res.status(500).json({
+              message: "An error occurred while generating the QR code",
+            });
+          }
+        }
+      }
     } catch (error) {
       res
         .status(500)
@@ -172,6 +238,11 @@ app.post(
     }
   }
 );
+
+//stranica koja pokazuje podatke o ulaznici prijavljenim korisnicima
+app.get("/ticket/:ticketId", async (req: Request, res: Response) => {
+  res.status(200).json({ message: "All good!" });
+});
 
 //definiranje rute za prikaz podataka
 app.get("/data", (req: Request, res: Response) => {
